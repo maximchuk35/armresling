6a1ed9d01ff2ae697df17c46e2325520186cf3ff
Саша Саша <maximchuk3@gmail.com>
2020-02-18 12:00:08 +0200

    создана модель користувача

---
+++ ClassLibrary1/model/Gender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibrary1.model
+{
+    public class Gender
+    { 
+        public string Name { get; }
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("imja ne moshe botu pystum abo nu4", nameof(name));
+
+            }
+
+            Name = name;
+
+            public override string ToString()
+        {
+            return Name;
+        }
+
+    }
+
+
+    }
+}
---
+++ ClassLibrary1/model/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.model
+{
+   public class User
+    {
+        public string Name { get;}
+        public Gender Gender { get; }
+        
+        public DateTime Birthdate { get; }
+        public double Weight { get; set; }
+
+        public double Height { get; set; }
+
+        public User(string name,
+            Gender gender,
+            DateTime birthDate,
+            double weight,
+            double height)
+        {
+            #region proverka ymov
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("imja ne moshe botu pystum abo nujakum", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("pol ne moshe botu pystum abo nujakum", nameof(gender));
+            }
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate >= DateTime.Now)
+            {
+                throw new ArgumentNullException("nemosh takoe pusatu", nameof(birthDate));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentNullException("vaga nemoshe botu mensha abo  nujaka ", nameof(weight));
+            }
+
+            if (height <= 0)
+            {
+                throw new ArgumentNullException("rost nemoshe botu mensha abo  nujaka ", nameof(height));
+            }
+            #endregion
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
